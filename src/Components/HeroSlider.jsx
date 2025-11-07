58d9d11af12146f41836dedb823f76e64ce8943a
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-11-07 21:30:06 +0600

    added mouse hover feature

--- src/Components/HeroSlider.jsx
+++ src/Components/HeroSlider.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router';
+import { AuthContext } from '../Auth/AuthProvider';
 
 const HeroSlider = () => {
+  const {user}=useContext(AuthContext);
+  const navigate=useNavigate();
     return (
         <div
   className="hero min-h-screen"
@@ -12,11 +16,13 @@ const HeroSlider = () => {
   <div className="hero-overlay"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <h1 className="mb-5 text-4xl font-bold">SkillSwap - A Local Skill Exchange Platform</h1>
       <p className="mb-5">
-        
+        An interactive platform for individuals to offer, learn, and trade skills within their local area. Whether it's language exchange, coding help, or physical exercise training — users can browse listings, rate experiences, and connect with local skill providers.
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      {
+        !user && <button onClick={()=>navigate("/auth/login")} className="btn btn-primary">Get Started</button>
+      }
     </div>
   </div>
 </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,10 @@ import { Link, NavLink } from 'react-router';
 import logo from '../assets/icons8-logo-50.png';
 import { AuthContext } from '../Auth/AuthProvider';
 import { toast } from 'react-toastify';
-
+import userIcon from '../assets/user.png';
 const Navbar = () => {
     const {user,logOut}=use(AuthContext);
+    console.log(user?.displayName);
     const handleLogOut=()=>{
         logOut()
         .then(()=>{
@@ -23,6 +24,13 @@ const Navbar = () => {
             <NavLink to="/profile">My Profile</NavLink>
             </div>
             <div className='flex gap-3'>
+                
+                <div className='relative group inline-block'>
+                    <img  className={`w-10 rounded-full cursor-pointer`} src={user ? user?.photoURL:userIcon} alt="User Profile" />
+                    {
+                        user && <span className='absolute top-10 left-1/2 -translate-x-1/2 text-sm rounded px-2 py-1 opacity-0 transition-opacity bg-gray-800 text-white  group-hover:opacity-100'>{user?.displayName}</span>
+                    }
+                </div>
                 {
                     user ?
                     <button onClick={handleLogOut} className='btn btn-neutral'>LogOut</button>
